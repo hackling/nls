0bedd13e805614c3970bac750a08026c03172f92
Jonathon M. Abbott <jma@dandaraga.net>
2013-09-29 13:21:34 +1000

    Fix navbar like a boss

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def navbar_li text, options
+    css_class = current_page?(options) ? "active" : ""
+    content_tag :li, class: css_class do
+      navbar_link_to text, options
+    end
+  end
+
+  private
+
+  def navbar_link_to text, path
+    link_to_unless_current text, path do
+      content_tag :a, text
+    end
+  end
 end
